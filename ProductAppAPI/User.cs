e2106379a65a8d09b201778e93153f1b827ef038
chingxsss <163365879+chingxsss@users.noreply.github.com>
2024-10-20 12:34:42 +0800

    Delete ProductAppAPI directory

--- ProductAppAPI/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProductManagement.API.Models
-{
-    public class User
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
-           
